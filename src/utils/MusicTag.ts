1a7bb5ff1b33fe23a94d171bdc212bb0734b5a6a
MyFaith <854372534@qq.com>
2025-02-15 00:58:57 +0800

    Add music metadata parsing with music-metadata library
    - Implement MusicTag utility for reading audio file metadata
    - Add music-metadata package to project dependencies
    - Create MusicTagInfo interface to standardize metadata extraction
    - Support reading various audio metadata properties like title, artists, album, etc.

--- src/utils/MusicTag.ts
+++ src/utils/MusicTag.ts
@@ -1,1 +1,34 @@
-export class MusicTag {}
+import { parseFile, type IAudioMetadata } from "music-metadata";
+
+export interface MusicTagInfo {
+  title?: string;
+  artists?: string[];
+  albumArtists?: string[];
+  album?: string;
+  year?: number;
+  diskNumber?: number;
+  trackNumber?: number;
+  genres?: string[];
+  comment?: string;
+  lyrics?: string;
+  cover?: string;
+}
+
+export class MusicTag {
+  static async read(filePath: string): Promise<MusicTagInfo> {
+    const metadata = await parseFile(filePath);
+    return {
+      title: metadata.common.title,
+      artists: metadata.common.artists,
+      albumArtists: metadata.common.albumartist?.split(","),
+      album: metadata.common.album,
+      year: metadata.common.year,
+      diskNumber: metadata.common.disk?.no ?? undefined,
+      trackNumber: metadata.common.track?.no ?? undefined,
+      genres: metadata.common.genre,
+      comment: metadata.common.comment?.join("\n"),
+      lyrics: metadata.common.lyrics?.join("\n"),
+      cover: metadata.common.picture?.[0]?.data.toString("base64")
+    };
+  }
+}
